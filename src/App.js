93bf6c8e45255ef360890a9039449db0a8b0734a
bmhe <bmhe@mobilepowered.fr>
2020-01-17 11:03:41 +0100

    add auth guard

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 import React from 'react'
 import './styles.css'
 import AppRoutes from './components/AppRoutes'
 
 export default function App () {
   return <AppRoutes />
-=======
-import React from "react";
-import "./styles.css";
-import Routes from "./routes";
-export default function App() {
-  return <Routes />;
->>>>>>> efeaf7471d9e5f9da05a27d13b6e36bf86253b45
 }
